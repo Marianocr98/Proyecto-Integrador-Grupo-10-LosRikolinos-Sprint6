44a948e08c89cbf3628bd860e7c51109a84674c9
Mayra Gutierrez <gutierrez.charcas97@gmail.com>
2022-02-16 17:46:30 -0300

    Mayra G. - Avanzando con el CRUD

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,10 +2,72 @@ const { validationResult} = require('express-validator');
 const bcrypt = require('bcrypt')
 
 
-const User = require('../model/user');
+/*const User = require('../model/user');*/
+
+const db = require('../database/models');
+
+//TRAE TODOS LOS USUARIOS
+
+db.User.findAll()
+.then(function(usuarios){
+    users = usuarios;
+})
 
 const userController = {
+
+    register: (req, res)=> {
+        res.render('./users/register');
+    },
+
+    processRegister: (req, res)=>{
+        /*
+        const resultadosValidos = validationResult(req);
+        if(resultadosValidos.errors.length > 0){
+            return res.render('./users/register', {
+                errors: resultadosValidos.mapped(),
+                oldData: req.body
+            });
+        }
+        let userInDB = User.findByField('email',req.body.email);
+        if(userInDB){
+            return res.render('../views/users/register.ejs',{
+                errors:{
+                    email :{
+                        msg: 'Este email ya esta registrado'
+                    }
+            },
+            oldData:req.body,
+        })
+        }
+        let body = req.body
+        delete body.confirm
+        let userToCreate = {
+            
+            ...body,
+            password: bcrypt.hashSync(req.body.password,10),
+            avatar:req.file.filename,
+            rol : "cliente"
+        }
+        let userCreated =  User.create(userToCreate)*/
+
+        db.User.create({
+            full_name: req.body.fullName,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10),
+            avatar: req.filename,
+            rol_id: 2
+        })
+        .then(function(user){
+            req.session.fullNameId = user.id;
+            req.session.fullName = user.fullName;
+            req.session.email = user.email;
+            req.session.avatar = user.avatar;
+            req.session.rol = 2;
+            req.session.save();
+        })
+        res.redirect('/login')
     
+    },
     login: (req, res)=> {
         // console.log(req.cookies);
         res.render('./users/login');
@@ -36,43 +98,6 @@ const userController = {
 			}	
 		})
 },
-
-    register: (req, res)=> {
-        res.render('./users/register');
-    },
-
-    processRegister: (req, res)=>{
-        const resultadosValidos = validationResult(req);
-        if(resultadosValidos.errors.length > 0){
-            return res.render('./users/register', {
-                errors: resultadosValidos.mapped(),
-                oldData: req.body
-            });
-        }
-        let userInDB = User.findByField('email',req.body.email);
-        if(userInDB){
-            return res.render('../views/users/register.ejs',{
-                errors:{
-                    email :{
-                        msg: 'Este email ya esta registrado'
-                    }
-            },
-            oldData:req.body,
-        })
-        }
-        let body = req.body
-        delete body.confirm
-        let userToCreate = {
-            
-            ...body,
-            password: bcrypt.hashSync(req.body.password,10),
-            avatar:req.file.filename,
-            rol : "cliente"
-        }
-        let userCreated =  User.create(userToCreate)
-        res.redirect('/login')
-    
-    },
     profile: (req, res)=> {
         res.render('./users/profile',
         {user: req.session.userLogged});
