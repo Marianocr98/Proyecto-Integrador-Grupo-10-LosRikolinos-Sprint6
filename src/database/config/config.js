a66e5add734467888f6a2695c81b1f3342c8e19b
Mayra Gutierrez <gutierrez.charcas97@gmail.com>
2022-02-22 12:54:16 -0300

    Mayra G.- Modificaciones de estilo

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
 module.exports = {
-  
   "development": {
-  "username": process.env.DB_USERNAME,
+  /*"username": process.env.DB_USERNAME,
   "password": process.env.DB_PASSWORD,
   "database": process.env.DB_DATABASE,
   "host": process.env.DB_HOST,
   "port": process.env.DB_PORT,
-  "dialect": process.env.DB_DIALECT
+  "dialect": process.env.DB_DIALECT,*/
+  username: "root",   
+  password: null,   
+  database: "rikolinos",   
+  host: "127.0.0.1",   
+  port: "3306",   
+  dialect: "mysql"
   },
 
   "production": {
@@ -17,6 +22,7 @@ module.exports = {
   "host": process.env.DB_HOST,
   "port": process.env.DB_PORT,
   "dialect": process.env.DB_DIALECT
-  }
+  },
+  
 
 }
