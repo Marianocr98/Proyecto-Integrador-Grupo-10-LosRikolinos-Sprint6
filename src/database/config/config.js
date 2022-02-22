ee5b96d6418bf55d2f3fefcc644dce8f6c9a8d13
Mariano Cespedez <marianocespedez1998@gmail.com>
2022-02-22 18:19:46 -0300

    Mariano Cespedez - Agregue cambios en el config.js y en el dotenv

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,18 +1,12 @@
 require('dotenv').config()
 module.exports = {
   "development": {
-  /*"username": process.env.DB_USERNAME,
+  "username": process.env.DB_USERNAME,
   "password": process.env.DB_PASSWORD,
   "database": process.env.DB_DATABASE,
   "host": process.env.DB_HOST,
   "port": process.env.DB_PORT,
-  "dialect": process.env.DB_DIALECT,*/
-  username: "root",   
-  password: null,   
-  database: "rikolinos",   
-  host: "127.0.0.1",   
-  port: "3306",   
-  dialect: "mysql"
+  "dialect": process.env.DB_DIALECT,
   },
 
   "production": {
