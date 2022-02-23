216f425c147b1ebe7c69d15ad44e5e6b43fdfd30
Mariano Cespedez <marianocespedez1998@gmail.com>
2022-02-23 14:23:47 -0300

    Mariano Cespedez - cambios en config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,21 +1,12 @@
 require('dotenv').config()
-<<<<<<< HEAD
 module.exports = {
   "development": {
-  // "username": process.env.DB_USERNAME,
-  // "password": process.env.DB_PASSWORD,
-  // "database": process.env.DB_DATABASE,
-  // "host": process.env.DB_HOST,
-  // "port": process.env.DB_PORT,
-  // "dialect": process.env.DB_DIALECT
-
-  username: 'root',
-  password: null,
-  database: 'rikolinos',
-  host: '127.0.0.1',
-  port: 3306,
-  dialect: 'mysql'
-
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_DATABASE,
+  "host": process.env.DB_HOST,
+  "port": process.env.DB_PORT,
+  "dialect": process.env.DB_DIALECT
   },
 
   "production": {
@@ -27,25 +18,4 @@ module.exports = {
   "dialect": process.env.DB_DIALECT
   },
   
-
-=======
-module.exports =
-{
-    "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT,
-},
-    "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT
-}
->>>>>>> fa2a9b882428e4a22577d382a667d97c6568db1a
 }
